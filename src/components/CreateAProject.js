b0bea35e8b6f01417bc1914a8a7be5d572101327
edda2022 <candyreinecke@googlemail.com>
2023-02-16 14:00:21 +0100

    sruff for the projectdetail page

---
+++ src/components/CreateAProject.js
@@ -0,0 +1,18 @@
+import { Helmet } from "react-helmet"
+
+export default function CreateAProject () {
+    return(
+        <>
+        <div className="create_project">CreateAProject
+        
+        
+        
+        </div>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Create A Project|CoCreateLab</title>
+            <link rel="canonical" href="/createproject" />
+        </Helmet>
+        </>
+    )
+}
